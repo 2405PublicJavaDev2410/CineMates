f7b604b59911ceedc5f8f2a1e515fcd0d3060bc2
KH2DL <lkh8937@gmail.com>
2024-10-07 14:09:31 +0900

    [ADD]movie-list html, css 추가

--- src/main/java/com/filmfellows/cinemates/app/movie/MovieContoller.java
+++ src/main/java/com/filmfellows/cinemates/app/movie/MovieContoller.java
@@ -1,11 +1,20 @@
 package com.filmfellows.cinemates.app.movie;
 
+import com.filmfellows.cinemates.domain.movie.model.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
 public class MovieContoller {
+
+    private final MovieService movieService;
+
+    @GetMapping("movie-list")
+    public String showMovieList(){
+        return "pages/movie/movieList";
+    }
 }
