522ce30eafb38a53a70ca01910f80368dc7eebc5
jun09211 <aa11216871@gmail.com>
2024-10-07 14:18:34 +0900

    [ADD] 극장 part 기능구현

---
+++ src/main/java/com/filmfellows/cinemates/domain/cinema/model/vo/Showtime.java
@@ -0,0 +1,26 @@
+package com.filmfellows.cinemates.domain.cinema.model.vo;
+
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+public class Showtime {
+    private int showtimeNo;
+    private int screenNo;
+    private int movieNo;
+    private String showtimeTime;
+    private int showtimeSeat;
+    private Date showtimeStart;
+    private Date showtimeEnd;
+
+    private int cinemaNo;
+    private String screenName;
+    private int screenSeat;
+    private String title;
+
+}
